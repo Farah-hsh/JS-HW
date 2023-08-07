5d118edcf0b88ec3d7f86821e1773b35cdfcd54e
Farah <farah.hsh.12@gmail.com>
2023-08-07 23:43:21 +0300

    day2

--- day2hw/main.js
+++ day2hw/main.js
@@ -3,15 +3,33 @@ let age=15
 let name="Farah"
 
 // practice2
-const age;
-age=15
+// const age;
+// age=15
 
-const name;
-age="Farah"
+// const name;
+// age="Farah"
 // const cannot be modified
 
 
 // practice3
+// trying whatever comes to mind nothing serious :)
+let user(
+    user="people"
+)
+users="change"
+console.log(user)
+
+
+const color(
+    color="whatever"
+)
+
+console.log=(color)
+
+// note: this is a practice and a trial 
+
+
+
 
 
 
