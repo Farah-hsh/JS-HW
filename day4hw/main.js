1482a3f4da6cb3ae47dbf131da7fe02ad73b1c54
Farah <farah.hsh.12@gmail.com>
2023-08-09 21:36:46 +0300

    day4 hw submitted

---
+++ day4hw/main.js
@@ -0,0 +1,66 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// day4 hw
+
+// practice1
+let str=String();
+let num= Number();
+let flo= parseFloat()
+
+let num2=42;
+let num2Tostr=String(num2);
+console.log(`${typeof num2Tostr}:${num2Tostr}`);
+
+
+let str2="25";
+let str2Tonum= Number(str2);
+console.log(`${typeof str2Tonum}:${str2Tonum}`);
+// end of practice1
+
+
+// practice2
+let str3="10";
+let str3Tonum=Number(str3);
+let math= (str3)-10;
+let math2= (str3)+10;
+
+console.log(`${typeof str3Tonum}:${str3Tonum}`);
+console.log(math , math2);
+// end of practice2
+
+
+// practice3
+let str4="15.5"
+let str4Toflo= parseFloat(str4);
+let math3=(str4)*10;
+let math4=(str4)/5;
+
+
+console.log(`${typeof str4Toflo}:${str4Toflo}`);
+console.log(math3 , math4);
+// end of practice3
+
+
+// practice4
+let str5="5";
+let num1=3;
+let math5= (str5)+(num1);
+
+console.log(math5);
+// end of practice4
+
+
+
+// practice5
+let boolean=true;
+let num3=1;
+let booleanTonum=Number(boolean);
+let math6=(boolean)+(num3);
+
+console.log(`${typeof booleanTonum}:${booleanTonum}`);
+console.log(math6);
+// end of practice5
+
+
+// day4 hw done
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
