2e41c64faabaffc13dac63ee41f8e8b403f729ea
Farah <farah.hsh.12@gmail.com>
2023-08-13 19:15:06 +0300

    day6 hw submitted

---
+++ day6 hw/main.js
@@ -0,0 +1,27 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// day6 hw
+
+// creating array
+let array=["farah","jumanah","alhanouf","ghalyah","aljowharah"];
+
+// print third name
+console.log(array.slice(2,3));
+
+// add name in the begining
+array.unshift("samah");
+console.log(array);
+
+// add name to the end
+array.push("ali");
+console.log(array);
+
+// remove first name
+array.shift();
+console.log(array);
+
+// remove last name
+array.pop();
+console.log(array);
+
+// done
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
