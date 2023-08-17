ae4f8bfb8a0469c3482ad51c90ef7ee65bf9fc46
Farah <farah.hsh.12@gmail.com>
2023-08-17 13:13:59 +0300

    day9 hw

---
+++ day9 hw/main.js
@@ -0,0 +1,37 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// day 9 hw loops
+
+
+// 1 
+for (let i = 1; i <= 30; i++) {
+    if (i <= 10) {   // don't put == for your own good ! no one likes infinity loops
+        console.log(i);
+    }
+}
+
+
+// 2
+for (let i = 1; i <= 20; i++) {
+    if (i != 10) {
+        console.log(i)
+    }
+}
+
+
+// 3
+let i = 1;
+while (i <= 3) {
+    console.log(i);
+    i++; 
+}
+
+
+// 4
+let f = 0;
+do{
+    console.log(f);
+    f++
+}while( f <= 5)
+
+// end of day 9 hw
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
