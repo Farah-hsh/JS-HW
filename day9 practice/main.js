19a2e3540feaf1939181868dc1bdd3929c83df43
Farah <farah.hsh.12@gmail.com>
2023-08-17 12:33:13 +0300

    day 9 practice

---
+++ day9 practice/main.js
@@ -0,0 +1,34 @@
+// day 9 practice
+
+
+// practice 1 
+// let age=prompt("how old are you ? ");
+
+// if(age < 18 ){
+//     alert("you  don't deserve a vacation");
+// }
+
+// let employee= prompt("are you an employee? ");
+// let doctor= prompt("are you a doctor? ");
+
+// if(age > 18 && employee||doctor){
+//     alert("you deserve a vacation");
+// }
+// end of practice 1 p1
+
+
+// practice 1 in another way
+let ages=prompt("how old are you ? ");
+
+if(ages < 18 ){
+    alert("you  don't deserve a vacation");
+}
+
+let work= prompt("what do you work? ");
+
+if(work == "docter"||"employee" ){
+    alert("you  deserve a vacation");
+}
+// end of practice 1 p2
+
+
