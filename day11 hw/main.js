325f2cea3c2d175a7c483c9dbd785e12a3acecc3
Farah <farah.hsh.12@gmail.com>
2023-08-21 17:25:08 +0300

    day11 done

---
+++ day11 hw/main.js
@@ -0,0 +1,19 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// day11 hw
+
+
+// print the first time with the first name and the second time with with the second name
+let javascript_Boot_camp = (teach) => {
+    return ("this javascript bootcamp with " + teach + " ok?")
+}
+console.log( javascript_Boot_camp ( "samah" ) ) ;
+console.log( javascript_Boot_camp ( "ali" ) ) ;
+
+
+
+// calculate the  mean
+let math = ( value1 , value2 ) => {
+    let value = value1 + value2
+    return value / 2 
+}
+console.log( math ( 10 , 20 ) ) ;
